07de5de8dec846738f02d467333b49a09c5d0d63
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-11-27 13:49:21 +0000

    Update website_alarm.py

--- website_alarm.py
+++ website_alarm.py
@@ -1,2 +1,13 @@
 import time
 import webbrowser
+
+alarm_set = input("Set the website alarm in the form of H:M:S: ")
+
+url = input("Enter URL you want to open when the alarm goes off: ")
+
+real_time = time.strftime("%I:%M:%S")
+
+while real_time != alarm_set:
+    print ("The time is " + real_time)
+    Actual_Time = time.strftime("%I:%M:%S")
+    time.sleep(1)
